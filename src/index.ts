12d8f885a4da629da4e99da346660396de51c2a8
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-03-05 09:48:45 +0100

    lesson start

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+//app.use("/users/:id/post", postRoutes )
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+//import {} from "../controllers/postController"
+
+const router = express.Router();
+
+
+// CRUD för en resurs posts
+// router.post("/users/:id/posts", createPostByUser);
+// router.get("/users/:id/posts", getPostsByUser);
+// router.get("/users/:id/posts/:id", getPostByUser);
+// router.put("/users/:id/posts/:id", updatePostByUser);
+// router.delete("/users/:id/posts/:id", deletePostByID);
+
+export default router;
+
+
